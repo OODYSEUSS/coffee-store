c796effef2bb083e55eb68a0ab12e3c6432cb943
oodyseuss <lucifer.zamess@gmail.com>
2024-04-14 19:01:30 +0300

    Added application structure and navigation settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,39 @@
+import 'package:coffee_store/screens/authentication/auth_screen.dart';
+import 'package:coffee_store/screens/authentication/login_screen.dart';
+import 'package:coffee_store/screens/authentication/register_screen.dart';
+import 'package:coffee_store/screens/authentication/reset_passw_screen.dart';
+import 'package:coffee_store/screens/home/home_screen.dart';
+import 'package:coffee_store/screens/onboarding/onboarding_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const Application(),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class Application extends StatelessWidget {
+  const Application({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const Auth(),
+        '/start': (context) => const OnboardingScreen(),
+        '/login': (context) => const Login(),
+        '/reset': (context) => const ResetPassword(),
+        '/register': (context) => const Register(),
+        '/home': (context) => const HomeScreen(),
+      },
     );
   }
 }
